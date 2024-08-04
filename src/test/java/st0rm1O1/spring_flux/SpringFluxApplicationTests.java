c420be6bfb0c59c36a8cbb1b5a63aaf49e494cde
Kunal Vartak <dev3@sprktechnologies.in>
2024-08-04 17:11:03 +0530

    Initial commit

---
+++ src/test/java/st0rm1O1/spring_flux/SpringFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package st0rm1O1.spring_flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
